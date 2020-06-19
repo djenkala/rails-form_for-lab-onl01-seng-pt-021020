971c8938cfed315aaac8e873501a91490f74469c
Daniel Jenkala <djenkala@yahoo.com>
2020-06-19 17:39:44 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126181722) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "school_classes", force: :cascade do |t|
     t.string   "title"
